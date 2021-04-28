87ef8678ff838389115a329dbdf8f257e2eee8a7
Ben Werdmuller <ben@benwerd.com>
2021-04-27 23:09:33 -0700

    Improved ID use

--- Main.php
+++ Main.php
@@ -8,9 +8,9 @@ namespace IdnoPlugins\Like {
         {
             \Idno\Core\Idno::site()->routes()->addRoute('/like/edit/?', '\IdnoPlugins\Like\Pages\Edit');
             \Idno\Core\Idno::site()->routes()->addRoute('/like/callback/?', '\IdnoPlugins\Like\Pages\Callback');
-            \Idno\Core\Idno::site()->routes()->addRoute('/like/edit/([A-Za-z0-9\-]+)/?', '\IdnoPlugins\Like\Pages\Edit');
-            \Idno\Core\Idno::site()->routes()->addRoute('/like/delete/([A-Za-z0-9\-]+)/?', '\IdnoPlugins\Like\Pages\Delete');
-            \Idno\Core\Idno::site()->routes()->addRoute('/bookmark/([A-Za-z0-9\-]+)/.*', '\Idno\Pages\Entity\View');
+            \Idno\Core\Idno::site()->routes()->addRoute('/like/edit/:id/?', '\IdnoPlugins\Like\Pages\Edit');
+            \Idno\Core\Idno::site()->routes()->addRoute('/like/delete/:id/?', '\IdnoPlugins\Like\Pages\Delete');
+            \Idno\Core\Idno::site()->routes()->addRoute('/bookmark/:id/.*', '\Idno\Pages\Entity\View');
         }
 
         function registerTranslations()
